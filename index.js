d8e1d78e95d9c4e5807b0fffee85509845e45573
SiveleleN <sivelelenkamane29@gmail.com>
2023-11-27 12:32:50 +0200

    commit

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 
         document.addEventListener('DOMContentLoaded', function() {
             
-            const booleanArray = [false, true, false, true, true, false, true, false, false, true, true, true, false];
+            let booleanArray = [false, true, false, true, true, false, true, false, false, true, true, true, false];
 
             
-            const trueCount = booleanArray.filter(Boolean).length;
+            let trueCount = booleanArray.filter(Boolean).length;
 
             
-            const resultElement = document.createElement('p');
-            resultElement.textContent = `Number of true values: ${trueCount}`;
+            let resultElement = document.createElement('h'); resultElement.textContent = `Number of true values: ${trueCount}`;
             document.body.appendChild(resultElement);
             
         });
